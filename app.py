1fa0300f9d9c328748eb4e7a94b3bb2d38d2d93a
ilhamAdhim <ilhamm179@gmail.com>
2019-12-15 13:28:16 +0800

    Added more documentation

--- app.py
+++ app.py
@@ -6,12 +6,13 @@ app = Flask(__name__)
 @app.route("/",methods=['POST','GET'])
 @app.route("/home",methods=['POST','GET'])
 def home():
+    # Return to the same page but with different brightness and electricity usage from html input
     if request.method == 'POST':
         brightness = main(request.form['KWHValue'])
         return render_template('lamp.html',
         brightness_value =  brightness,
         KWHValue = request.form['KWHValue'])
-
+    # Default configuration
     return render_template('lamp.html',
     brightness_value =  50,
     KWHValue = 500)
